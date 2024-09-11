9f20a24550346b80157f03da74da64e0a74f97e5
Dyw4n <zagajni@gmail.com>
2024-09-11 14:34:52 +0200

    wstawianie do tablicy losowych i ich wypisywanie

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+
+        int [] tablicalosowa = new int [6];
+        for (int i = 0; i <tablicalosowa.length; i++) {
+            tablicalosowa[i] = (int) (Math.random()*100+1);
+        }
+        System.out.println("wylosowana tablica");
+        for (int element:tablicalosowa) {
+
+            System.out.print(element + ", ");
+        }
     }
-}git
+}
